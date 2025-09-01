096a9f7629d15b07d4bcc27373e1c065f5dbd8e2
roguisharcanetrickster <roguisharcanetrickster@gmail.com>
2025-09-01 20:28:30 +0700

    more info on pwa run

--- test_cases/pwa_form.js
+++ test_cases/pwa_form.js
@@ -1,6 +1,8 @@
 export default () => {
    describe("Text", () => {
       it("Exists", () => {
+         cy.visit("/mobile/app/admin/c355634c-42ca-4317-a086-3aeb4f750d8b?route=First_Page");
+         cy.get("#app").should("exist");
          // Select the Text tab
          cy.get('.page-content')
             .contains("Example Text in a custom widget")
