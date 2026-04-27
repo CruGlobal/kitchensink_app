7318beaf639ad32e6449ac5f41cd143133eca960
roguisharcanetrickster <roguisharcanetrickster@gmail.com>
2026-04-27 15:41:02 +0700

    add kanban

---
+++ test_cases/widget_kanban.js
@@ -0,0 +1,16 @@
+export default (folderName) => {
+   describe("Chart", () => {
+      beforeEach(() => {
+         cy.visit("/");
+         cy.get(
+            '[data-cy="tab-Kanban-b5657df0-bc8a-4fc8-bd00-8c0928932ffc-b52e6e96-5033-4c7f-a104-29bd5ddcac4a"]',
+         ).click();
+      });
+
+      it("Display kanban", () => {
+         cy.log("Displays a kanban");
+         // check the tag area
+         cy.findByText('Test-KCS-0000000004').should("exist");
+      });
+   });
+};
